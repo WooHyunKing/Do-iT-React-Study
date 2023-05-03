1eedf94b8809ad531e05ca55960ecd8d0746796e
신 우현 <woo_hyun_1@naver.com>
2023-05-03 19:29:21 +0900

    5-1. Final code of useEffect lecture

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Timer from "./components/Timer";
 function App() {
   const [name, setName] = useState("킹우현");
   const [age, setAge] = useState(25);
-
   const [show, setShow] = useState(false);
 
   // 렌더링 될 때마다 실행됨(componentDidMount + 모든 State에 대해 componentDidUpdate)
@@ -14,17 +13,17 @@ function App() {
     console.log("렌더링 🎨");
   });
 
-  // 최초 렌더링(마운트) + age가 변화할 때마다 실행됨(componentDidMount + 특정 값 componentDidUpdate)
+  // // 최초 렌더링(마운트) + age가 변화할 때마다 실행됨(componentDidMount + 특정 값 componentDidUpdate)
   useEffect(() => {
     console.log("age 변화 🕓");
   }, [age]);
 
-  // 최초 렌더링(마운트) + name이 변화할 때마다 실행됨(componentDidMount + 특정 값 componentDidUpdate)
+  // // 최초 렌더링(마운트) + name이 변화할 때마다 실행됨(componentDidMount + 특정 값 componentDidUpdate)
   useEffect(() => {
     console.log("name 변화 ✨");
   }, [name]);
 
-  // 최초 렌더링 시에만 실행됨(componentDidMount)
+  // // 최초 렌더링 시에만 실행됨(componentDidMount)
   useEffect(() => {
     console.log("첫 렌더링(Mount) ⭐️");
   }, []);
